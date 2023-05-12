bbcbba0c3cbe53d77aded36c9df4e8ae4e67f2be
Jakub Pawlowski <jakub.pawlowski00@gmail.com>
2023-05-12 05:05:38 +0200

    Removed body padding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,98 +2,96 @@
 </script>
 
 <template>
-    <div class="container mx-auto">
-        <div class="grid gap-4 grid-cols-1">
-            <div class="flex gap-4">
-                <div class="flex-auto w-1/5 card card-image h-96 image-full">
-                    <figure>
-                        <img
-                            class="object-center"
-                            src="/profilepicture.jpg"
-                            alt="Profile picture"
-                        />
-                    </figure>
-                </div>
-                <div class="flex-auto w-4/5 card card-about h-96">
-                    <div class="card-body">
-                        <h2 class="card-title text-5xl">
-                            Hey, name's Jakub and I'm a Warsaw based Fullstack Developer.
-                        </h2>
-                        <p class="">
-                            Currently studying at Polish-Japanese Academy of Information Technology,
-                            my wide range of skills in all areas of software development are perfect
-                            for the needs of your company!
-                        </p>
-                        <div class="card-actions">
-                            <a target="_blank" href="https://www.linkedin.com/in/jakubpawlowski00/">
-                                <button class="btn gap-2">
-                                    <font-awesome-icon icon="fa-brands fa-linkedin" size="xl" />
-                                    Contact me
-                                </button>
-                            </a>
-                            <a target="_blank" href="https://www.github.com/manymanecki">
-                                <button class="btn btn-outline gap-2">
-                                    <font-awesome-icon
-                                        icon="fa-brands fa-square-github"
-                                        size="xl"
-                                    />
-                                    GitHub
-                                </button>
-                            </a>
-                        </div>
+    <div class="grid gap-4 grid-cols-1">
+        <div class="flex gap-4">
+            <div class="flex-auto w-1/5 card card-image h-96 image-full">
+                <figure>
+                    <img
+                        class="object-center"
+                        src="/profilepicture.jpg"
+                        alt="Profile picture"
+                    />
+                </figure>
+            </div>
+            <div class="flex-auto w-4/5 card card-about h-96">
+                <div class="card-body">
+                    <h2 class="card-title text-5xl">
+                        Hey, name's Jakub and I'm a Warsaw based Fullstack Developer.
+                    </h2>
+                    <p class="">
+                        Currently studying at Polish-Japanese Academy of Information Technology,
+                        my wide range of skills in all areas of software development are perfect
+                        for the needs of your company!
+                    </p>
+                    <div class="card-actions">
+                        <a target="_blank" href="https://www.linkedin.com/in/jakubpawlowski00/">
+                            <button class="btn gap-2">
+                                <font-awesome-icon icon="fa-brands fa-linkedin" size="xl" />
+                                Contact me
+                            </button>
+                        </a>
+                        <a target="_blank" href="https://www.github.com/manymanecki">
+                            <button class="btn btn-outline gap-2">
+                                <font-awesome-icon
+                                    icon="fa-brands fa-square-github"
+                                    size="xl"
+                                />
+                                GitHub
+                            </button>
+                        </a>
                     </div>
                 </div>
             </div>
-            <div class="flex flex-auto">
-                <div class="card card-about h-96">
-                    <div class="card-body">
-                        <h2 class="card-title text-5xl">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </h2>
-                        <p class="">
-                            In ut erat elit. Etiam et malesuada risus. Curabitur iaculis nunc
-                            egestas, interdum erat id, lobortis velit. Quisque interdum tortor at
-                            metus laoreet auctor.
-                        </p>
-                    </div>
-                </div>
-                <div class="card card-about h-96">
-                    <div class="card-body">
-                        <h2 class="card-title text-5xl">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </h2>
-                        <p class="">
-                            In ut erat elit. Etiam et malesuada risus. Curabitur iaculis nunc
-                            egestas, interdum erat id, lobortis velit. Quisque interdum tortor at
-                            metus laoreet auctor.
-                        </p>
-                    </div>
+        </div>
+        <div class="flex flex-auto">
+            <div class="card card-about h-96">
+                <div class="card-body">
+                    <h2 class="card-title text-5xl">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </h2>
+                    <p class="">
+                        In ut erat elit. Etiam et malesuada risus. Curabitur iaculis nunc
+                        egestas, interdum erat id, lobortis velit. Quisque interdum tortor at
+                        metus laoreet auctor.
+                    </p>
                 </div>
             </div>
-            <div class="flex flex-auto">
-                <div class="card card-about h-96">
-                    <div class="card-body">
-                        <h2 class="card-title text-5xl">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </h2>
-                        <p class="">
-                            In ut erat elit. Etiam et malesuada risus. Curabitur iaculis nunc
-                            egestas, interdum erat id, lobortis velit. Quisque interdum tortor at
-                            metus laoreet auctor.
-                        </p>
-                    </div>
+            <div class="card card-about h-96">
+                <div class="card-body">
+                    <h2 class="card-title text-5xl">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </h2>
+                    <p class="">
+                        In ut erat elit. Etiam et malesuada risus. Curabitur iaculis nunc
+                        egestas, interdum erat id, lobortis velit. Quisque interdum tortor at
+                        metus laoreet auctor.
+                    </p>
                 </div>
-                <div class="card card-about h-96">
-                    <div class="card-body">
-                        <h2 class="card-title text-5xl">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        </h2>
-                        <p class="">
-                            In ut erat elit. Etiam et malesuada risus. Curabitur iaculis nunc
-                            egestas, interdum erat id, lobortis velit. Quisque interdum tortor at
-                            metus laoreet auctor.
-                        </p>
-                    </div>
+            </div>
+        </div>
+        <div class="flex flex-auto">
+            <div class="card card-about h-96">
+                <div class="card-body">
+                    <h2 class="card-title text-5xl">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </h2>
+                    <p class="">
+                        In ut erat elit. Etiam et malesuada risus. Curabitur iaculis nunc
+                        egestas, interdum erat id, lobortis velit. Quisque interdum tortor at
+                        metus laoreet auctor.
+                    </p>
+                </div>
+            </div>
+            <div class="card card-about h-96">
+                <div class="card-body">
+                    <h2 class="card-title text-5xl">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </h2>
+                    <p class="">
+                        In ut erat elit. Etiam et malesuada risus. Curabitur iaculis nunc
+                        egestas, interdum erat id, lobortis velit. Quisque interdum tortor at
+                        metus laoreet auctor.
+                    </p>
                 </div>
             </div>
         </div>
